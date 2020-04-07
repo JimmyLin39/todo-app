567af4de78240bd6c86609b6a8c4843d721468a6
JimmyLin39 <jimmylin0309@email.com>
2020-04-07 14:29:01 -0700

    add initial categories componet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Todos from './components/Todos'
+import Categories from './components/Categories'
 
 function App() {
   return (
     <div className='App'>
+      <Categories />
       <Todos />
     </div>
   )
