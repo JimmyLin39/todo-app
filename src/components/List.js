4ac26d7bb95e5ee88178963355627b187f281dcc
JimmyLin39 <jimmylin0309@email.com>
2020-04-07 16:49:25 -0700

    add Task and display

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function List() {
+export default function List({ remove, items }) {
   return (
     <ul>
-      <li>list</li>
+      {items.map(item => (
+        <li key={item.id}>
+          <span>{item.name}</span>
+          <button onClick={() => remove(item)}>X</button>
+        </li>
+      ))}
     </ul>
   )
+}
+
+List.propTypes = {
+  remove: PropTypes.func.isRequired,
+  items: PropTypes.object.isRequired
 }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,14 +1,40 @@
 import React from 'react'
 import List from './List'
+import { addTodoAction, removeTodoAction, toggleTodoAction } from '../actions'
+import { generateId } from '../util'
 
-export default function Todos(props) {
+export default function Todos({ store, todos }) {
+  let taskInput
+  let categoryInput
+
+  const addTask = e => {
+    e.preventDefault()
+    const name = taskInput.value
+    taskInput.value = ''
+
+    store.dispatch(
+      addTodoAction({
+        id: generateId(),
+        name,
+        complete: false
+      })
+    )
+  }
   return (
     <>
       <h1>Todo List</h1>
-      <List />
-      <button>+</button>
-      <input type='text' placeholder='Add Task' />
-      <input type='text' placeholder='Add Category' />
+      <List items={todos} />
+      <button onClick={addTask}>+</button>
+      <input
+        type='text'
+        placeholder='Add Task'
+        ref={node => (taskInput = node)}
+      />
+      <input
+        type='text'
+        placeholder='Add Category'
+        ref={node => (categoryInput = node)}
+      />
     </>
   )
 }
