9a14157a43873e7be43018bc61f8093c735d28d7
JimmyLin39 <jimmylin0309@email.com>
2020-04-08 11:56:45 -0700

    update with material ui

--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -3,27 +3,50 @@ import PropTypes from 'prop-types'
 import { makeStyles } from '@material-ui/core/styles'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ListItemText from '@material-ui/core/ListItemText'
+import FormGroup from '@material-ui/core/FormGroup'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/checkbox'
+import Chip from '@material-ui/core/Chip'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper
+  }
+}))
 
 export default function TodoItems({ items, remove, toggle }) {
+  const classes = useStyles()
+
   return (
-    <ul>
-      {items.map(item => (
-        <li key={item.id}>
-          <span
-            onClick={() => toggle(item.id)}
-            style={{
-              textDecoration: item.complete ? 'line-through' : 'none'
-            }}
-          >
-            {item.name}
-          </span>{' '}
-          | {item.category && <span>{item.category}</span>}
-          <button onClick={() => remove(item.id)}>X</button>
-        </li>
-      ))}
-    </ul>
+    <div className={classes.root}>
+      <List component='nav' aria-label='Todo items'>
+        {items.map(item => (
+          <ListItem key={item.id}>
+            {item.category && <Chip label={item.category} color='primary' />}
+
+            <FormGroup row>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={item.complete}
+                    onChange={() => toggle(item.id)}
+                    name={item.name}
+                    color='primary'
+                  />
+                }
+                label={item.name}
+                style={{
+                  textDecoration: item.complete ? 'line-through' : 'none'
+                }}
+              />
+            </FormGroup>
+            <button onClick={() => remove(item.id)}>X</button>
+          </ListItem>
+        ))}
+      </List>
+    </div>
   )
 }
 
